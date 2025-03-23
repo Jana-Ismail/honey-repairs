08dba95b833828399ab19b810870056b1ecbf790
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-23 01:18:33 -0500

    Login and Register components | ApplictionViews and Authorized to enable application routing only for logged in users | 'logout' Link in NavBar to clear local storage and route back to '/login' view | currentUser state in ApplicationViews to enable prop drilling to pass down logged in user info

--- src/services/userService.js
+++ src/services/userService.js
@@ -17,3 +17,19 @@ export const getEmployeeUserByUserId = async (userId) => {
     const response = await fetch(`http://localhost:8088/users/${userId}?_embed=employees`)
     return await response.json()
 }
+
+export const getUserByEmail = (email) => {
+  return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+    res.json()
+  )
+}
+
+export const createUser = (customer) => {
+  return fetch("http://localhost:8088/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  }).then((res) => res.json())
+}
