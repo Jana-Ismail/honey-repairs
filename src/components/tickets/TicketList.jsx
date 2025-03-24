cfaccd010aa2dff1e366d87fb706693602057583
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-24 02:24:08 -0500

    Changed logic to conditionally render filter buttons in the TicketFilterBar based on whether the current user is an employee or a customer | added JSX for 'Create Ticket', 'Open Tickets', and 'All My Tickets' filter buttons in customer TicketFilterBar | useState() hook for showOpenOnly state | onClick functions on 'Open Tickets' and 'All My Tickets' buttons to set state to true or false | useEffect() hook to setFilteredTickets based on showOpenOnly state

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -9,7 +9,8 @@ export const TicketList = ({ currentUser }) => {
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
     const [searchTerm, setSearchTerm] = useState("")
-  
+    const [showOpenTicketsOnly, setShowOpenTicketsOnly] = useState(false)
+
     const getAndSetTickets = async () => {
       const tickets = await getAllTickets()
       if (currentUser.isStaff) {
@@ -33,6 +34,15 @@ export const TicketList = ({ currentUser }) => {
     }, [showEmergencyOnly, allTickets])
 
     useEffect(() => {
+      if (showOpenTicketsOnly) {
+        const openTickets = allTickets.filter(ticket => !ticket.dateCompleted)
+        setFilteredTickets(openTickets)
+      } else {
+        setFilteredTickets(allTickets)
+      }
+    }, [showOpenTicketsOnly, allTickets])
+
+    useEffect(() => {
       const foundTickets = allTickets.filter(ticket => ticket.description.toLowerCase().includes(searchTerm.toLowerCase()))
       setFilteredTickets(foundTickets)
     }, [searchTerm, allTickets])
@@ -40,11 +50,21 @@ export const TicketList = ({ currentUser }) => {
     return (
       <div className="tickets-container">
         <h2>Tickets</h2>
-        <TicketFilterBar setShowEmergencyOnly={setShowEmergencyOnly} setSearchTerm={setSearchTerm}/>
+        <TicketFilterBar 
+          setShowEmergencyOnly={setShowEmergencyOnly} 
+          setSearchTerm={setSearchTerm} 
+          currentUser={currentUser}
+          setShowOpenTicketsOnly={setShowOpenTicketsOnly} 
+        />
         <article className="tickets">
           {filteredTickets?.map(ticketObj => {
             return (
-                <Ticket ticket={ticketObj} key={ticketObj.id} currentUser={currentUser} setTickets={getAndSetTickets}/>
+                <Ticket 
+                  ticket={ticketObj} 
+                  key={ticketObj.id} 
+                  currentUser={currentUser} 
+                  setTickets={getAndSetTickets}
+                />
             )
           })}
         </article>
