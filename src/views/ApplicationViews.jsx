e241957300b01655c865cc8c58bf3888851366fd
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-24 01:24:09 -0500

    Set up CustomerViews '/' Route with an Outlet and a nested index Route to render the Welcome component | Set up CustomerNavBar to render above the Outlet Routes nested in '/' Route | Set up 'Logout' Link in the CustomerNavBar

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,13 +1,4 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { Welcome } from "../components/welcome/welcome"
-import { TicketList } from "../components/tickets/TicketList"
-import { CustomerList } from "../components/customers/CustomerList"
-import { CustomerDetails } from "../components/customers/CustomerDetails"
-import { EmployeeList } from "../components/employees/EmployeeList"
-import { EmployeeDetails } from "../components/employees/EmployeeDetails"
-import { NavBar } from "../components/navbar/NavBar"
 import { useEffect, useState } from "react"
-import { EmployeeForm } from "../components/forms/EmployeeForm"
 import { EmployeeViews } from "./EmployeeViews"
 import { CustomerViews } from "./CustomerViews"
 
